96afc0caafbd8cd1ce4d73f674b1bfff1f57d60b
Oyelaja Hussein <premiumitng@gmail.com>
2024-08-20 14:42:04 +0200

    Added some update

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const weddingDay = new Date("October 26, 2024 10:10am");
+const weddingDay = new Date("2024-10-26T10:10:00");
 const x = setInterval(function () {
   const currentDate = new Date();
   const distance = weddingDay - currentDate;
 
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  if (distance >= 0) {
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("countDown").innerText = 
-    `Event in ${days}d : ${hours}h : ${minutes}m : ${seconds}s `;
- 
-    
-}, 1000); // This function will run every second
+    document.getElementById(
+      "countDown"
+    ).innerText = `Event in ${days}d : ${hours}h : ${minutes}m : ${seconds}s `;
+  } else {
+    document.getElementById("countDown").innerText = "The event has passed.";
+    clearInterval(x);
+  }
+}, 1000);
